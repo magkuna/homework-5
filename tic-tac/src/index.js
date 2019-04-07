eb62b6b47d726d83609bdaf0cb2d3477cc35f85d
magkuna <magkuna@gmail.com>
2019-04-07 19:34:27 +0200

    changes in render

--- tic-tac/src/index.js
+++ tic-tac/src/index.js
@@ -38,7 +38,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: X' + (this.state.xIsNext ? 'X' : '0')
 
     return (
       <div>
