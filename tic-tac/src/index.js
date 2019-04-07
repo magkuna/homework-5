2381267623763f8f9df447a659cdba07dcb87fe7
magkuna <magkuna@gmail.com>
2019-04-07 19:53:15 +0200

    recording the tic-tac-toe game’s history

--- tic-tac/src/index.js
+++ tic-tac/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 
 import './index.css'
 
-
-
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
@@ -14,20 +12,6 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
   renderSquare(i) {
     return (
       <Square
@@ -65,11 +49,28 @@ class Game extends React.Component {
     super(props);
     this.state = {
       history: [{
-        squares: Array(9).fill(null),
+        squares: Array(9).fill(null)
       }],
-      xIsNext: true,
+      xIsNext: true
     };
   }
+
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares
+      }]),
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+  
   render() {
     const history = this.state.history;
     const current = history[history.length - 1];
@@ -85,13 +86,13 @@ class Game extends React.Component {
     return (
       <div className="game">
         <div className="game-board">
-        <Board
+          <Board
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
           />
         </div>
         <div className="game-info">
-        <div>{status}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
