6249a097cb58c5bb7a8567e977f8a0c7a12d31ce
magkuna <magkuna@gmail.com>
2019-04-07 19:56:45 +0200

    key element solved

--- tic-tac/src/index.js
+++ tic-tac/src/index.js
@@ -78,10 +78,10 @@ class Game extends React.Component {
 
     const moves = history.map((step, move) => {
       const desc = move ?
-        'Go to move #' + move :
+        'Go to move number ' + move :
         'Go to game start';
       return (
-        <li>
+        <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       )
