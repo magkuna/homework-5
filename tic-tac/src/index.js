d0c55706c0b46c911d209d03a6405884b9de07a2
magkuna <magkuna@gmail.com>
2019-04-07 19:48:45 +0200

    game component render function

--- tic-tac/src/index.js
+++ tic-tac/src/index.js
@@ -14,7 +14,7 @@ function Square(props) {
 }
 
 class Board extends React.Component {
- 
+
 
   handleClick(i) {
     const squares = this.state.squares.slice();
@@ -31,8 +31,8 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
@@ -80,13 +80,27 @@ class Game extends React.Component {
     };
   }
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+        <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+        <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
